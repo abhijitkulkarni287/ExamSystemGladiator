61af72a862338fa8ad0b077a33a9dd1826c2f009
Brijesh Verma <brijeshverma0007@gmail.com>
2019-11-21 23:54:42 +0530

    new branch

--- Exam_010_JPA_Module/src/com/lti/core/daos/ExamDao.java
+++ Exam_010_JPA_Module/src/com/lti/core/daos/ExamDao.java
@@ -3,7 +3,6 @@ package com.lti.core.daos;
 import java.util.List;
 
 import com.lti.core.entities.ExaminationDetails;
-import com.lti.core.entities.QuestionDetails;
 
 /*
  * DAO for ExaminationDetails Table
