fd4744e8fd3f195b34d7697860d80a92868ccb98
Abhijit Kulkarni <{abhijitkulkarni.287@gmail.com}>
2019-11-21 19:56:14 -1200

    imp: Choices class insertion

---
+++ Exam_010_JPA_Module/src/com/lti/core/services/AdminQuestionService.java
@@ -0,0 +1,10 @@
+package com.lti.core.services;
+
+/*
+ * Provides service for admin operations of adding and deleting questions
+ */
+public interface AdminQuestionService 
+{
+	public void addQuestionsFromFile();
+
+}
---
+++ Exam_010_JPA_Module/src/com/lti/core/services/AdminQuestionServiceImpl.java
@@ -0,0 +1,79 @@
+package com.lti.core.services;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.lti.core.entities.QuestionDetails;
+
+public class AdminQuestionServiceImpl implements AdminQuestionService 
+{
+
+	@Override
+	public void addQuestionsFromFile() 
+	{
+		//1) Extraction of data from excel file
+		try 
+		{
+			//excel file location
+			String filePath="C:/Users/AE103_PC2/Desktop/AB NOTES/EmployeeDetails.xlsx";
+			FileInputStream file = new FileInputStream(filePath);
+			//getting workbook
+			XSSFWorkbook questionWorkbook = new XSSFWorkbook(file);
+			//getting the specific sheet in the workbook
+			XSSFSheet questionSheet= questionWorkbook.getSheetAt(0);
+			//creating an array list for employee details
+			List<QuestionDetails> questionList = new ArrayList<QuestionDetails>();
+			//extracting record from every row and inserting it into the list
+			for(int i=questionSheet.getFirstRowNum()+1;i<=questionSheet.getLastRowNum();i++)
+			{
+				//autowire this later
+				QuestionDetails questions = new QuestionDetails();
+				
+				
+				Row row = questionSheet.getRow(i);
+				System.out.println();
+				for(int j=row.getFirstCellNum();j<row.getLastCellNum();j++)
+				{
+						Cell cell = row.getCell(j);	
+							//numeric cell type
+							if(cell.getCellType()==0)
+							{
+								//correct option can be numeric
+								if(cell.getColumnIndex()==1){questions.setCorrectOption(Double.toString(cell.getNumericCellValue()));}
+								
+								//for adding choices if numeric
+								
+							}
+							//alpha numeric cell type
+							else
+							{
+								//for setting question
+								if(cell.getColumnIndex()==0){questions.setQuestion(cell.getStringCellValue());}
+								//for setting correctOption
+								if(cell.getColumnIndex()==1){questions.setCorrectOption(cell.getStringCellValue());}
+								
+								//for adding choices if string
+								
+							}
+					
+				}
+			}
+			file.close();
+			
+			//printing employee list
+			
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
